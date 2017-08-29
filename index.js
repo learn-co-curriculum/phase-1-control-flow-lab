6742cae367d1ba9e3cdbf51cb8803817c2d1970c
Jeff Katz <jeff@flatironschool.com>
2017-08-29 09:37:05 -0400

    add solution to branch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return 'This one is on me!'
+  } else if (1999 < feet && feet < 2500){
+      return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}
