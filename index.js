4d3beb9b569c1ca4a8738c1e68a70d15cc6a5589
Gabe Jackson <gj@mail.co.de>
2017-09-25 16:53:35 -0400

    Update code style and remove unnecessary break statement

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 // Write your code in this file!
-function scuberGreetingForFeet(feet){
-  if(feet <= 400){
-    return 'This one is on me!'
-  } else if (1999 < feet && feet < 2500){
-      return "I will gladly take your thirty bucks."
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
   } else {
-    return "No can do."
+    return "No can do.";
   }
 }
 
-function ternaryCheckCity(city){
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip (tip) {
   switch (tip) {
     case 'generous':
-      return 'Thank you so much.'
-      break;
+      return 'Thank you so much.';
     case 'not as generous':
-      return 'Thank you.'
+      return 'Thank you.';
     default:
-      return 'Bye.'
+      return 'Bye.';
   }
 }
