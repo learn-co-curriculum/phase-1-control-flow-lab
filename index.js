5e499a513ed5251341a0425518cf07fe0caf097e
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-19 14:47:25 -0600

    commit tree 9812766806aeddeab39ded695cde502360047e42

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet >= 2000 && feet <= 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if (feet >= 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const daCity = city == 'NYC' ? 'Ok, sounds good.' : 'No go.' ;
+  return daCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
