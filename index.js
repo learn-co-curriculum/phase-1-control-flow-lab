4b38ce8d55edaa09c8e097291d48e6c81e8961cc
Caner <kurucaner@gmail.com>
2021-08-09 17:05:56 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(result){
+  if (result <= 400){
+    return "This one is on me!";
+  }  else if (result >= 2000 && result <2499) {
+    return "I will gladly take your thirty bucks.";
+  } else if (result >= 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let thecity = city === 'NYC' ? 'Ok, sounds good.' : "No go.";
+  return (thecity)
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case'generous':
+     return "Thank you so much.";
+     break;
+    case'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
+}   
