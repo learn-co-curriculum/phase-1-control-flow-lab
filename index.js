3f746f3e327c5b25dfd9c27d01e4ae66a23aecc6
Ibaihima <ibaihima@hotmail.com>
2022-01-31 09:48:34 -0500

    commit tree b5732cb3ac72b2e8b942d2f150931efc4b7f7844

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400 )
+  return "This one is on me!"
+  if (feet > 1999 && feet < 2500)
+  return "I will gladly take your thirty bucks."
+  else
+  return "No can do."
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if  (city === "NYC")
+  return "Ok, sounds good."
+  else 
+  return "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if  (tip === "generous")
+  return "Thank you so much."
+  if (tip === "not as generous" )
+  return "Thank you."
+  else 
+  return "Bye."
 }
