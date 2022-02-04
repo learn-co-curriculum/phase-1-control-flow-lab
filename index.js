8278a4e6a6d11317de055dcc6c3538cc07c0cc1e
omarabbasi <omarkabbasi@outlook.com>
2022-02-03 17:36:13 -0800

    commit tree 44be7deaf816714c196fb50c50a8085c16831746

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(sample){
   // Write your code here!
+  if (400 >= sample) {
+  return "This one is on me!"
+ } else if (sample > 2000 && sample < 2500) {
+   return ("I will gladly take your thirty bucks.")
+ } else return ("No can do.")
 }
+ 
+  function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+ 
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  // Write your code here
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
