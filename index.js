c3a49d51a38a20e64e22449b1c684a9194d62442
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-08 13:37:59 -0400

    first function pass

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  if(value <= 400){
+    return "This one is on me!";
+  } if(value > 400 && value <= 2000) {
+    return "That will be twenty bucks.";
+  } if(value > 2000 && value < 2500 ) {
+    return "I will gladly take your thirty bucks."
+  }
+  
 }
 
 function ternaryCheckCity(){
