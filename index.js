e90c58b7c177fe2e16947e5ce47dd07c08878e3e
Amira de Moya <demoyaamira@gmail.com>
2023-02-17 13:48:56 -0500

    commit tree 1d9d9ee13eb609eb0ad60523bcf3729c7de380cb

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// learned how to use && to compare a range  from https://www.codegrepper.com/code-examples/c/javascript+between+range
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400)
+    return "This one is on me!"
+  else if ((someNumber > 400) && (someNumber <=2000))
+    return 'That will be twenty bucks.'
+  else if ((someNumber > 2000) && (someNumber < 2500))
+     return 'I will gladly take your thirty bucks.'
+  else (someNumber > 2500)
+   return "No can do."
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city){
+return city === "NYC" ? "Ok, sounds good." : "No go."
+
 }
+ 
+
+
+// switchOnCharmFromTip(){
+//   // Write your code here!
+// }
